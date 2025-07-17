90ad9bbe82c648190b77dd8c0fbec0830ec3d1d9
aviraj0403 <aviraj0403@gmail.com>
2025-07-18 01:36:51 +0530

    fix: Update API base URLs for consistency across environment configurations

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://grocery-backend-9jjx.onrender.com/api";
+const baseURL = "https://institute-backend-8u6d.onrender.com/api";
 // const baseURL = "http://localhost:5001/api"; // Local development
 const Axios = axios.create({
   baseURL,
